40a600cea4dee374c9c99a15002221dd805dbe17
Hampton Jacobs <hammyjacobs@gmail.com>
2022-03-04 17:01:48 -0500

    refactoring design

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -2,19 +2,27 @@ import React from 'react'
 import homecss from './Home.module.css'
 import Layout from '../../components/Layout/Layout'
 import AboutMe from '../AboutMe/AboutMe.jsx'
-import selfie from '/Users/hamptonjacobs/thunder_ga/portfolio/Portfolio/client/images/images/Propic_org copy.png'
+import Projects from '../Projects/Projects'
+// import selfie from '/Users/hamptonjacobs/thunder_ga/portfolio/Portfolio/client/images/images/Propic_org copy.png'
 export default function Home() {
+
+  const handleNavigate = (event) => {
+
+  }
+
   return (
     <Layout>
       <div className={homecss.home_body}>
         <div className={homecss.welcome}>
-          Hello {<br />} I am Hampton. <br /> Software Developer.
-
+          Hampton Jacobs {<br />} Software Developer.
         </div>
-        <AboutMe />
-        <img src={selfie} alt="Hampton" />
 
-        {/* <AboutMe /> */}
+        <div className={homecss.work}>
+          <button>See My Work</button>
+        </div>
+        <div className={homecss.me}>
+          <button>Get To Know Me</button>
+        </div>
       </div>
 
     </Layout>
--- client/src/screens/Projects/Projects.jsx
+++ client/src/screens/Projects/Projects.jsx
@@ -3,35 +3,33 @@ import Layout from '../../components/Layout/Layout'
 import projectcss from './Projects.module.css';
 export default function Projects() {
   return (
-    <Layout>
-      <div className={projectcss.whole}>
-        <div>
-          Dynamic
-          <p>React app that allows users to accord their training.</p>
-          <p>
-            React, Ruby on Rails, JavaScript, Component Styling
-          </p>
-          <a href="https://github.com/hammyhammer/dynamic">Repo</a>
-          <a href="https://621f874f3bd9090007f373bb--quizzical-jang-12a296.netlify.app/">Deployed Link</a>
-        </div>
-        <div>
-          Gameterest
-          <p>A platform where users can leave reviews and favorite videogames.</p>
-          <p>
-            React, Mongoose, MongoDB, Express, Component Styling.
-          </p>
-          <a href="https://github.com/Jacobdye99/Gameterest_BackEnd">Backend Repo</a>
-          <a href="https://github.com/Elvedin123/gametrest-frontend">Frontend Repo</a>
-          <a href="https://gameterest2.herokuapp.com/">Deployed Link</a>
-        </div>
-        <div>
-          Self-Check
-          <p>A react app where users can take a series of self-assessment health exams to gain insight to their health.</p>
-          <p>Javascript, CSS, HTML</p>
-          <a href="https://github.com/hammyhammer/bmi-calculator">Repo</a>
-          <a href="https://61e6d494910579b53712d385--unruffled-hermann-51aab3.netlify.app/">Deployed Site</a>
-        </div>
+    <div className={projectcss.whole}>
+      <div>
+        Dynamic
+        <p>React app that allows users to accord their training.</p>
+        <p>
+          React, Ruby on Rails, JavaScript, Component Styling
+        </p>
+        <a href="https://github.com/hammyhammer/dynamic">Repo</a>
+        <a href="https://621f874f3bd9090007f373bb--quizzical-jang-12a296.netlify.app/">Deployed Link</a>
       </div>
-    </Layout>
+      <div>
+        Gameterest
+        <p>A platform where users can leave reviews and favorite videogames.</p>
+        <p>
+          React, Mongoose, MongoDB, Express, Component Styling.
+        </p>
+        <a href="https://github.com/Jacobdye99/Gameterest_BackEnd">Backend Repo</a>
+        <a href="https://github.com/Elvedin123/gametrest-frontend">Frontend Repo</a>
+        <a href="https://gameterest2.herokuapp.com/">Deployed Link</a>
+      </div>
+      <div>
+        Self-Check
+        <p>A react app where users can take a series of self-assessment health exams to gain insight to their health.</p>
+        <p>Javascript, CSS, HTML</p>
+        <a href="https://github.com/hammyhammer/bmi-calculator">Repo</a>
+        <a href="https://61e6d494910579b53712d385--unruffled-hermann-51aab3.netlify.app/">Deployed Site</a>
+      </div>
+    </div>
   )
 }
