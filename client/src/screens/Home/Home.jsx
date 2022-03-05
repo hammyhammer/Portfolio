5b8f284b105c4153c61ceb7a3a55fef61226c29b
Hampton Jacobs <hammyjacobs@gmail.com>
2022-03-04 22:11:46 -0500

    images displaying

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import homecss from './Home.module.css'
 import Layout from '../../components/Layout/Layout'
-import AboutMe from '../AboutMe/AboutMe.jsx'
-import Projects from '../Projects/Projects'
-// import selfie from '/Users/hamptonjacobs/thunder_ga/portfolio/Portfolio/client/images/images/Propic_org copy.png'
 export default function Home() {
-
-  const handleNavigate = (event) => {
-
-  }
-
   return (
     <Layout>
       <div className={homecss.home_body}>
         <div className={homecss.welcome}>
-          Hampton Jacobs {<br />} Software Developer.
+          Hampton Jacobs. {<br />} Software Developer.
         </div>
 
         <div className={homecss.work}>
-          <button>See My Work</button>
+          <button>My Work</button>
         </div>
         <div className={homecss.me}>
           <button>Get To Know Me</button>
--- client/src/screens/Projects/Projects.jsx
+++ client/src/screens/Projects/Projects.jsx
@@ -1,35 +1,39 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import projectcss from './Projects.module.css';
+import dynamic from '../../assets/images/dynamic.png'
 export default function Projects() {
   return (
-    <div className={projectcss.whole}>
-      <div>
-        Dynamic
-        <p>React app that allows users to accord their training.</p>
-        <p>
-          React, Ruby on Rails, JavaScript, Component Styling
-        </p>
-        <a href="https://github.com/hammyhammer/dynamic">Repo</a>
-        <a href="https://621f874f3bd9090007f373bb--quizzical-jang-12a296.netlify.app/">Deployed Link</a>
+    <Layout>
+      <div className={projectcss.whole}>
+        <div>
+          Dynamic
+          <p>React app that allows users to accord their training.</p>
+          <img className={projectcss.image} src={dynamic} alt="dynamic screenshot" />
+          <p>
+            React, Ruby on Rails, JavaScript, Component Styling
+          </p>
+          <a href="https://github.com/hammyhammer/dynamic">Repo</a>
+          <a href="https://621f874f3bd9090007f373bb--quizzical-jang-12a296.netlify.app/">Deployed Link</a>
+        </div>
+        <div>
+          Gameterest
+          <p>A platform where users can leave reviews and favorite videogames.</p>
+          <p>
+            React, Mongoose, MongoDB, Express, Component Styling.
+          </p>
+          <a href="https://github.com/Jacobdye99/Gameterest_BackEnd">Backend Repo</a>
+          <a href="https://github.com/Elvedin123/gametrest-frontend">Frontend Repo</a>
+          <a href="https://gameterest2.herokuapp.com/">Deployed Link</a>
+        </div>
+        <div>
+          Self-Check
+          <p>A react app where users can take a series of self-assessment health exams to gain insight to their health.</p>
+          <p>Javascript, CSS, HTML</p>
+          <a href="https://github.com/hammyhammer/bmi-calculator">Repo</a>
+          <a href="https://61e6d494910579b53712d385--unruffled-hermann-51aab3.netlify.app/">Deployed Site</a>
+        </div>
       </div>
-      <div>
-        Gameterest
-        <p>A platform where users can leave reviews and favorite videogames.</p>
-        <p>
-          React, Mongoose, MongoDB, Express, Component Styling.
-        </p>
-        <a href="https://github.com/Jacobdye99/Gameterest_BackEnd">Backend Repo</a>
-        <a href="https://github.com/Elvedin123/gametrest-frontend">Frontend Repo</a>
-        <a href="https://gameterest2.herokuapp.com/">Deployed Link</a>
-      </div>
-      <div>
-        Self-Check
-        <p>A react app where users can take a series of self-assessment health exams to gain insight to their health.</p>
-        <p>Javascript, CSS, HTML</p>
-        <a href="https://github.com/hammyhammer/bmi-calculator">Repo</a>
-        <a href="https://61e6d494910579b53712d385--unruffled-hermann-51aab3.netlify.app/">Deployed Site</a>
-      </div>
-    </div>
+    </Layout>
   )
 }
