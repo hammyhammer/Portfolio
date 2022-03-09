fc5f405fc03db99441c83c97ccaa0e4a31e35d27
Hampton Jacobs <hammyjacobs@gmail.com>
2022-03-09 14:53:45 -0500

    cleaned up footer

--- client/src/components/Nav/Navbar.jsx
+++ client/src/components/Nav/Navbar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import navcss from './Navbar.module.css';
+import resume from '../../assets/resume/jacobs-resume.pdf'
 
 export default function Navbar() {
   return (
@@ -7,7 +8,12 @@ export default function Navbar() {
       <div className={navcss.links}>
         <NavLink className={navcss.individual_links} to="/">Home</NavLink>
         <a className={navcss.individual_links} href="#about-me">About Me</a>
-        <a className={navcss.individual_links} href="#resume">Resume</a>
+        <a
+          className={navcss.individual_links}
+          href={resume}
+          target="blank"
+          id="resume"
+        >Resume</a>
       </div>
     </div>
   )
