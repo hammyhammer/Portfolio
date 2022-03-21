de6da343536d9ae0d547315c39afae6f52f86955
Hampton Jacobs <hammyjacobs@gmail.com>
2022-03-21 12:31:54 -0400

    modified the navbar to include name

--- client/src/components/Nav/Navbar.jsx
+++ client/src/components/Nav/Navbar.jsx
@@ -4,7 +4,9 @@ import resume from '../../assets/resume/jacobs-resume.pdf'
 export default function Navbar() {
   return (
     <div className={navcss.nav}>
+
       <div className={navcss.links}>
+        <span className={navcss.name}>Hampton Jacobs</span>
         <a className={navcss.individual_links} href="#home">Projects</a>
         <a className={navcss.individual_links} href="#about-me">About Me</a>
         <a
