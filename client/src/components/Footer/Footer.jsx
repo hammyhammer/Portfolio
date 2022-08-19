dce18ecdfb02dabb9be064962c9e662a43859074
Hampton Jacobs <hammyjacobs@gmail.com>
2022-08-19 17:02:02 -0400

    added code to open a new window when github or ln icon clicked on

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 export default function Footer() {
   return (
     <div className={footercss.footer}>
-      <a href="https://github.com/hammyhammer">
+      <a href="https://github.com/hammyhammer" target="_blank" rel="noreferrer noopener">
         <p className={footercss.git}> <BsGithub size={24} /></p>
         {/* <img src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png" alt="Github Icon"
           className={footercss.git} /> */}
       </a>
-      <a href="https://www.linkedin.com/in/hampton-jacobs/">
+      <a href="https://www.linkedin.com/in/hampton-jacobs/" target="_blank" rel="noreferrer noopener">
         <p className={footercss.link}><BsLinkedin size={24} /></p>
         {/* <img src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png" alt="Linkedln Icon" */}
         {/* className={footercss.link} /> */}
