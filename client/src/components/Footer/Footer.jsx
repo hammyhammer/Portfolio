e965e2572dae61a6ae853ed86ad16780e1e61d06
Hampton Jacobs <hammyjacobs@gmail.com>
2022-08-19 17:02:47 -0400

    cleaned up footer jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,13 +5,9 @@ export default function Footer() {
     <div className={footercss.footer}>
       <a href="https://github.com/hammyhammer" target="_blank" rel="noreferrer noopener">
         <p className={footercss.git}> <BsGithub size={24} /></p>
-        {/* <img src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png" alt="Github Icon"
-          className={footercss.git} /> */}
       </a>
       <a href="https://www.linkedin.com/in/hampton-jacobs/" target="_blank" rel="noreferrer noopener">
         <p className={footercss.link}><BsLinkedin size={24} /></p>
-        {/* <img src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png" alt="Linkedln Icon" */}
-        {/* className={footercss.link} /> */}
       </a>
     </div>
   )
