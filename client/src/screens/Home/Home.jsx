303e732fdf7708d575acbfe775e9a5e19f9ca973
Hampton Jacobs <hammyjacobs@gmail.com>
2022-08-24 15:22:38 -0400

    commented out original header

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <Layout>
       <div className={homecss.home_body}>
-        <div className={homecss.welcome} id="home">
+        {/* <div className={homecss.welcome} id="home">
           Hampton Jacobs
-        </div>
-        <div className={homecss.traits}><p className={homecss.driven}>Driven</p><p className={homecss.bullet}>{"\u2022"}</p><p className={homecss.creative}>Creative</p><p className={homecss.bullet}>{"\u2022"}</p><p className={homecss.software}>Software Developer</p> </div>
+        </div> */}
+        {/* <div className={homecss.traits}><p className={homecss.driven}>Driven</p><p className={homecss.bullet}>{"\u2022"}</p><p className={homecss.creative}>Creative</p><p className={homecss.bullet}>{"\u2022"}</p><p className={homecss.software}>Software Developer</p> </div> */}
         <AboutMe />
         <Projects />
 
