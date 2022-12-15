8f85f01ba7b11358dfcd5d404a88890b413470ff
hammyhammer <hammyjacobs@gmail.com>
2022-12-14 22:43:53 -0500

    adjusted subheader

--- client/src/screens/AboutMe/AboutMe.jsx
+++ client/src/screens/AboutMe/AboutMe.jsx
@@ -7,7 +7,8 @@ export default function AboutMe() {
     <div className={aboutcss.whole} id="about-me">
       <div className={aboutcss.aboutme_container}>
         <div className={aboutcss.left_side}>
-          <h5 className={aboutcss.header}>Hi, I'm Hampton Jacobs. Driven, creative software Developer</h5>
+          <h5 className={aboutcss.header}>Hi, I'm Hampton Jacobs.</h5>
+          <p className={aboutcss.header}>Driven, creative software developer.</p>
           <p className={aboutcss.intro}>
             Through collaboration and initiative, I bring teammates together to problem solve and complete challenging tasks. My background in athletics and coaching gives me strengths in leadership, concise communication, adaptability, teamwork, and an empathetic mindset in team settings. When faced with a difficult problem, I see it as an opportunity for me to learn and to ask questions to expand my knowledge.
           </p>
