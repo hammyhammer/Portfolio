bb1be21dd063940407b3589fa4fcc610c9d2a855
hammyhammer <hammyjacobs@gmail.com>
2022-12-14 22:46:25 -0500

    added TS icon

--- client/src/screens/Skills/Skills.jsx
+++ client/src/screens/Skills/Skills.jsx
@@ -8,6 +8,7 @@ export default function Skills() {
       <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" alt="css3" />
       <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" alt="html5" />
       <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="react" />
+      <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="typescript" />
       {/* <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="gitHub" /> */}
       <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg" alt="mongoDB" />
       <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain-wordmark.svg" alt="ruby" />
@@ -16,6 +17,8 @@ export default function Skills() {
       <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg" alt="node.js" />
       <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain-wordmark.svg" alt="postgresql" />
       <img className={skillscss.icons} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain-wordmark.svg" alt="heroku" />
+
+
     </div>
   )
 }
